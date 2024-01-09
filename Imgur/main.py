3d56e5f6a950aed2b9d61d0ef7e95e0ac8a915f9
Răzvan Fișer <razvan.fiser@gmail.com>
2024-01-09 13:49:59 +0200

    Cleaned up responses from Imgur and Sports microservices

--- Imgur/main.py
+++ Imgur/main.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from prometheus_client import Histogram, Counter, generate_latest, CONTENT_TYPE_LATEST
 from time import time
 import requests
+import urllib
 
 # from config import config
 import psycopg2
@@ -12,8 +13,8 @@ SECRET = "027f7378b9e15c0aae455b292380a5bfba5ba71a"
 app = Flask(__name__)
 
 @app.route('/search')
-def get_event_at_date():
-    sort, window, page = "time", "all", 1
+def search_for_term():
+    sort, window, page = "top", "all", 1
     query = request.args.get("q")
     url = f"https://api.imgur.com/3/gallery/search/{sort}/{page}?q_all={query}"
 
@@ -22,8 +23,11 @@ def get_event_at_date():
     }
 
     response = requests.get(url, headers=headers)
+    # print(response.status_code, response.json())
+    data = response.json()["data"]
+    out = {"images": [data[i]["link"] for i in range(len(data))]}
 
-    return response.json(), response.status_code
+    return jsonify(out), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- Sports/main.py
+++ Sports/main.py
@@ -52,20 +52,22 @@ def metrics():
 
 @app.route('/events/<int:sport_id>/live', methods=["GET"])
 def get_live_events(sport_id):
-    try:
-        url = f"https://sportscore1.p.rapidapi.com/sports/{sport_id}/events/live"
+    url = f"https://sportscore1.p.rapidapi.com/sports/{sport_id}/events/live"
 
-        querystring = {"page": "1"}
+    querystring = {"page": "1"}
 
-        headers = {
-            "X-RapidAPI-Key": "6e297b3aa8mshee38d1a1c1c6f1ep11483ejsn6602bee3a178",
-            "X-RapidAPI-Host": "sportscore1.p.rapidapi.com"
-        }
+    headers = {
+        "X-RapidAPI-Key": "6e297b3aa8mshee38d1a1c1c6f1ep11483ejsn6602bee3a178",
+        "X-RapidAPI-Host": "sportscore1.p.rapidapi.com"
+    }
 
-        response = requests.get(url, headers=headers, params=querystring)
-        return response.json(), response.status_code
-    except Exception as e:
-        return jsonify({"Error": str(e)}), 500
+    response = requests.get(url, headers=headers, params=querystring)
+    r_json = response.json()["data"]
+    out = [{"away_score": r_json[i]["away_score"],
+           "home_score": r_json[i]["home_score"],
+           "away_team": r_json[i]["away_team"],
+           "home_team": r_json[i]["home_team"]} for i in range(len(r_json))]
+    return jsonify(out), response.status_code
 
 @app.route('/events/date/<string:date>', methods=["GET"])
 def get_event_at_date(date):
@@ -79,8 +81,12 @@ def get_event_at_date(date):
     }
 
     response = requests.get(url, headers=headers, params=querystring)
-
-    return response.json(), response.status_code
+    r_json = response.json()["data"]
+    out = [{"away_score": r_json[i]["away_score"],
+            "home_score": r_json[i]["home_score"],
+            "away_team": r_json[i]["away_team"],
+            "home_team": r_json[i]["home_team"]} for i in range(len(r_json))]
+    return jsonify(out), response.status_code
 
 @app.route('/teams/<int:sport_id>', methods=["GET"])
 def get_teams_by_id(sport_id):
@@ -94,8 +100,12 @@ def get_teams_by_id(sport_id):
     }
 
     response = requests.get(url, headers=headers, params=querystring)
-
-    return response.json(), response.status_code
+    r_json = response.json()["data"]
+    out = [{"name": r_json[i]["name"],
+            "country": r_json[i]["country"],
+            "gender": r_json[i]["gender"],
+            "sport_id": r_json[i]["sport_id"]} for i in range(len(r_json))]
+    return jsonify(out), response.status_code
 
 @app.route('/players/<int:sport_id>', methods=["GET"])
 def get_players_by_id(sport_id):
@@ -109,8 +119,15 @@ def get_players_by_id(sport_id):
     }
 
     response = requests.get(url, headers=headers, params=querystring)
-
-    return response.json(), response.status_code
+    r_json = response.json()["data"]
+    out = [{"name": r_json[i]["name"],
+            "main_team": r_json[i]["main_team"],
+            "date_of_birth": r_json[i]["date_birth_at"],
+            "height": r_json[i]["height"],
+            "rating": r_json[i]["rating"],
+            "market_value": r_json[i]["market_value"],
+            "sport_id": r_json[i]["sport_id"]} for i in range(len(r_json))]
+    return jsonify(out), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
